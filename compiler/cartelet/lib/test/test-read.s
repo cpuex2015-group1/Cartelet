203f79a4fb632309fc6cf774c8146621acd6c70c
nekketsuuu <nekketsuuu@users.noreply.github.com>
2016-01-05 19:48:15 +0900

    Fix a bug of read library (compiler)

--- compiler/cartelet/lib/test/test-read.s
+++ compiler/cartelet/lib/test/test-read.s
@@ -31,13 +31,12 @@ min_caml_read_float_c1:
 .globl	main
 main:
 	addi	%r29 %r0 $1023
-	addi	%r25 %r0 $10
-	sll	%r29 %r29 %r25
+	slli	%r29 %r29 $10
 	addi	%r29 %r29 $1023
 	addi	%r28 %r0 $1023
 	addi	%r2 %r0 $1
 	jal	min_caml_read_float
-	fmov	%f31 %f0
+	fmov	%f31 %f2
 	jal	min_caml_read_float
 	halt
 # read_int (32bit, ASCII -> byte)
@@ -52,41 +51,34 @@ min_caml_read_int:
 	addi	%r3 %r0 $0
 	# 最初の1bitはマイナスかもしれない
 	addi	%r11 %r0 $0x2d  # '-'
-	addi	%r12 %r0 min_caml_read_int_start
 min_caml_read_int_start:
 	recv8	%r3
 	beq	%r3 %r11 min_caml_read_int_negative
-	slt	%r13 %r3 %r9
-	slt	%r14 %r10 %r3
-	add	%r13 %r13 %r14
-	beq	%r13 %r0 min_caml_read_int_positive
-	jr	%r12
+	ble	%r9 %r3 min_caml_read_int_start_ble_30
+	beq	%r0 %r0 min_caml_read_int_start
+min_caml_read_int_start_ble_30:
+	ble	%r3 %r10 min_caml_read_int_positive
+	beq	%r0 %r0 min_caml_read_int_start
 min_caml_read_int_negative:
 	addi	%r8 %r0 $1
 	addi	%r2 %r0 $0
-	addi	%r11 %r0 min_caml_read_int_loop
-	jr	%r11
+	beq	%r0 %r0 min_caml_read_int_loop
 min_caml_read_int_positive:
 	addi	%r8 %r0 $0
 	addi	%r2 %r3 $-48  # ASCII to binary
 min_caml_read_int_loop:
 	# recieve
 	recv8	%r3
-	slt	%r11 %r3 %r9
-	bneq	%r11 %r0 min_caml_read_int_sign
-	slt	%r11 %r10 %r3
-	bneq	%r11 %r0 min_caml_read_int_sign
+	blt	%r3 %r9 min_caml_read_int_sign
+	blt	%r10 %r3 min_caml_read_int_sign
 	# multiply by 10
-	addi	%r13 %r0 $1
-	sll	%r11 %r2 %r13
-	addi	%r13 %r0 $3
-	sll	%r12 %r2 %r13
+	slli	%r11 %r2 $1
+	slli	%r12 %r2 $3
 	add	%r2 %r11 %r12
 	# add a digit
 	addi	%r3 %r3 $-48
 	add	%r2 %r2 %r3
-	addi	%r11 %r0 min_caml_read_int_loop
-	jr	%r11
+	beq	%r0 %r0 min_caml_read_int_loop
 min_caml_read_int_sign:
 	# 符号判定
 	beq	%r8 %r0 min_caml_read_int_exit
@@ -99,186 +91,228 @@ min_caml_read_float:
 	# float_of_intの分の誤差も入ることに注意。
 	# 整数も受け取る。".1"(= 0.1)みたいなのには対応していない。
 	addi	%r29 %r29 $-2
-	st	0(%r29) %r31
+	sw	0(%r29) %r31
 	jal	min_caml_read_int
-	st	1(%r29) %r3  # r3の区切り文字情報をストアしておく
+	sw	1(%r29) %r3  # r3の区切り文字情報をストアしておく
 	jal	min_caml_float_of_int
-	ld	0(%r29) %r31
+	lw	%r31 0(%r29)
 	addi	%r29 %r29 $2
-	ld	-1(%r29) %r8
+	lw	%r8 -1(%r29)
 	addi	%r9 %r0 $0x2e  # '.'
 	bneq	%r8 %r9 min_caml_read_float_exit
 	# ここから小数点以下
 	# 1.1と1.01を区別するため、read_intは使えない
-	# r2: ans, r3: #(head zeros) r8: buffer, r9: '0', r10: '9', r11: temp
-	# f0: integer part
+	# r2: ans, r3: #(head zeros) r8: buffer, r9: '0', r10: '9'
+	# f2: integer part
 	addi	%r9 %r0 $0x30
 	addi	%r10 %r0 $0x39
-	addi	%r11 %r0 min_caml_read_float_loop1
 	addi	%r3 %r0 $0
 	addi	%r8 %r0 $0
 	# 先頭の0を数えつつ上桁を探す
 min_caml_read_float_loop1:
 	recv8	%r8
-	slt	%r12 %r8 %r9
-	bneq	%r12 %r0 min_caml_read_float_exit
-	slt	%r12 %r10 %r8
-	bneq	%r12 %r0 min_caml_read_float_exit
+	blt	%r8 %r9 min_caml_read_float_exit
+	blt	%r10 %r8 min_caml_read_float_exit
 	bneq	%r8 %r9 min_caml_read_float_loop1_exit
 	addi	%r3 %r3 $1
-	jr	%r11
+	beq	%r0 %r0 min_caml_read_float_loop1
 min_caml_read_float_loop1_exit:
 	addi	%r2 %r8 $-48
 min_caml_read_float_loop2:
 	# recieve
 	recv8	%r8
-	slt	%r11 %r8 %r9
-	bneq	%r11 %r0 min_caml_read_float_loop2_exit
-	slt	%r11 %r10 %r8
-	bneq	%r11 %r0 min_caml_read_float_loop2_exit
+	blt	%r8 %r9 min_caml_read_float_loop2_exit
+	blt	%r10 %r8 min_caml_read_float_loop2_exit
 	# multiply by 10
-	addi	%r13 %r0 $1
-	sll	%r11 %r2 %r13
-	addi	%r13 %r0 $3
-	sll	%r12 %r2 %r13
+	slli	%r11 %r2 $1
+	slli	%r12 %r2 $3
 	add	%r2 %r11 %r12
 	# add a digit
 	addi	%r8 %r8 $-48
 	add	%r2 %r2 %r8
-	addi	%r11 %r0 min_caml_read_float_loop2
-	jr	%r11
+	beq	%r0 %r0 min_caml_read_float_loop2
 min_caml_read_float_loop2_exit:
-	fst	-1(%r29) %f0
-	st	-2(%r29) %r3
+	fsw	-1(%r29) %f2
+	sw	-2(%r29) %r3
 	addi	%r29 %r29 $-3
-	st	0(%r29) %r31
+	sw	0(%r29) %r31
 	jal	min_caml_float_of_int
-	ld	0(%r29) %r31
+	lw	%r31 0(%r29)
 	addi	%r29 %r29 $3
-	fld	-1(%r29) %f1
-	ld	-2(%r29) %r3
+	flw	%f3 -1(%r29)
+	lw	%r3 -2(%r29)
 	# 小数点以下の部分を0.fffまでずらし、更に頭の0の個数だけずらす
 	addi	%r8 %r0 min_caml_float_1
-	fld	0(%r8) %f2
+	flw	%f4 0(%r8)
 	addi	%r8 %r0 min_caml_read_float_c1
-	fld	0(%r8) %f3
-	addi	%r8 %r0 min_caml_read_float_loop3
+	flw	%f5 0(%r8)
 min_caml_read_float_loop3:
 	# r3: #(head zero)
-	# f0: 小数点以下, f1: 整数部分, f2: 1.0f, f3: 0.1f
-	fslt	%f0 %f2
-	bclt	min_caml_read_float_loop3_exit
-	fmul	%f0 %f0 %f3
-	jr	%r8
-min_caml_read_float_loop3_exit:
-	addi	%r8 %r0 min_caml_read_float_loop4
+	# f2: 小数点以下, f3: 整数部分, f4: 1.0f, f5: 0.1f
+	fblt	%f2 %f4 min_caml_read_float_loop4
+	fmul	%f2 %f2 %f5
+	beq	%r0 %r0 min_caml_read_float_loop3
 min_caml_read_float_loop4:
-	slt	%r9 %r0 %r3
-	beq	%r9 %r0 min_caml_read_float_loop4_exit
+	ble	%r3 %r0 min_caml_read_float_loop4_exit
 	addi	%r3 %r3 $-1
-	fmul	%f0 %f0 %f3
-	jr	%r8
+	fmul	%f2 %f2 %f5
+	beq	%r0 %r0 min_caml_read_float_loop4
 min_caml_read_float_loop4_exit:
 	# 符号の兼ね合いを見つつ整数部分と小数点以下の部分を足す
-	addi	%r8 %r0 min_caml_float_0
-	fld	0(%r8) %f2
-	fslt	%f1 %f2
-	bclf	min_caml_read_float_loop_exit_positive
-	fneg	%f0 %f0
+	fble	%f0 %f3 min_caml_read_float_loop_exit_positive
+	fneg	%f2 %f2
 min_caml_read_float_loop_exit_positive:
-	fadd	%f0 %f0 %f1
+	fadd	%f2 %f2 %f3
 min_caml_read_float_exit:
 	jr	%r31
 # read_int_byte (32bit, byte -> byte)
 min_caml_read_int_byte:
-	addi	%r8 %r0 $8
 	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
 	jr	%r31
 # read_float_byte (32bit, byte -> byte)
 min_caml_read_float_byte:
-	addi	%r8 %r0 $8
 	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
-	sll	%r2 %r2 %r8
-	recv8	%r2
-	st	-1(%r29) %r2
-	fld	-1(%r29) %f0
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
+	slli	%r2 %r2 $8
+	recv8	%r3
+	add	%r2 %r2 %r3
+	sw	-1(%r29) %r2
+	flw	%f2 -1(%r29)
+	jr	%r31
+##
+## libmincaml_int_float.S
+## むっちゃ大きい数に対してはまだむちゃくちゃ遅い！
+## 加算を倍々に足すやつにすべき(あとでする)
+##
+
+# int_of_float (a.k.a. truncate)
+min_caml_int_of_float:
+min_caml_truncate:
+	# FLAGを決めてabsをする
+	fblt	%f2 %f0 min_caml_int_of_float_flag_negative
+	addi	%r8 %r0 $0
+	beq	%r0 %r0 min_caml_int_of_float_after_flag
+min_caml_int_of_float_flag_negative:
+	addi	%r8 %r0 $1
+	fneg	%f2 %f2
+min_caml_int_of_float_after_flag:
+	addi	%r9 %r0 min_caml_float_int_c2
+	flw	%f3 0(%r9)
+	fble	%f3 %f2 min_caml_int_of_float_big
+	# |x| < 8388608.0
+	# r2: answer, r8: FLAG, r9: addr, r10: imm, r11: const for shift
+	# f2: |x|, f3: 8388608.0
+	fadd	%f2 %f2 %f3
+	fsw	-1(%r29) %f2
+	lw	%r2 -1(%r29)
+	addiu32	%r10 %r0 $0x4b000000
+	sub	%r2 %r2 %r10
+	# FLAGの調整
+	beq	%r8 %r0 min_caml_int_of_float_small_positive
+	sub	%r2 %r0 %r2
+min_caml_int_of_float_small_positive:
+	jr	%r31
+min_caml_int_of_float_big:
+	# |x| >= 8388608.0
+	# r2: answer, r8: FLAG, r9: addr, r11: 8388608
+	# f2: |x|, f3: 8388608.0, f4: -8388608.0
+	fneg	%f4 %f3
+	addi	%r2 %r0 $0
+	addiu32 %r11 %r0 $8388608
+	# m回8388608を足す
+min_caml_int_of_float_big_loop:
+	fadd	%f2 %f2 %f4
+	add	%r2 %r2 %r11
+	fble	%f3 %f2 min_caml_int_of_float_big_loop
+	# int_of_float(n)を足す
+	sw	-1(%r29) %r2
+	addi	%r29 %r29 $-2
+	sw	0(%r29) %r31
+	jal	min_caml_int_of_float
+	lw	%r31 0(%r29)
+	addi	%r29 %r29 $2
+	lw	%r3 -1(%r29)
+	add	%r2 %r2 %r3
+	# FLAGの調整
+	beq	%r8 %r0 min_caml_int_of_float_big_positive
+	sub	%r2 %r0 %r2
+min_caml_int_of_float_big_positive:
 	jr	%r31
 # float_of_int
 min_caml_float_of_int:
 	# FLAGを決めてabsをする
-	slt	%r8 %r2 %r0
-	beq	%r8 %r0 min_caml_float_of_int_flag_positive
+	ble	%r0 %r2 min_caml_float_of_int_flag_positive
 	addi	%r9 %r0 $1
 	sub	%r11 %r0 %r2
-	addi	%r10 %r0 min_caml_float_of_int_after_flag
-	jr	%r10
+	beq	%r0 %r0 min_caml_float_of_int_after_flag
 min_caml_float_of_int_flag_positive:
 	addi	%r9 %r0 $0
 	add	%r11 %r0 %r2
 min_caml_float_of_int_after_flag:
 	addiu32	%r8 %r0 $8388608
-	slt	%r10 %r11 %r8
-	beq	%r10 %r0 min_caml_float_of_int_big
+	ble	%r8 %r11 min_caml_float_of_int_big
 	# |x| < 838860
 	# 8388608.0f + xにして、8388608.0fを引く
 	# r2: x, r8: const or addr, r9: FLAG, r10: temp, r11: |x|
-	# f0: answer
+	# f2: answer
 	addiu32	%r8 %r0 $0x4b000000
 	add	%r10 %r11 %r8
-	st	-1(%r29) %r10
-	fld	-1(%r29) %f0
+	sw	-1(%r29) %r10
+	flw	%f2 -1(%r29)
 	addi	%r8 %r0 min_caml_float_int_c1
-	fld	0(%r8) %f1
-	fadd	%f0 %f0 %f1
+	flw	%f3 0(%r8)
+	fadd	%f2 %f2 %f3
 	# FLAG
 	beq	%r9 %r0 min_caml_float_of_int_small_positive
-	fneg	%f0 %f0
+	fneg	%f2 %f2
 min_caml_float_of_int_small_positive:
 	jr	%r31
 min_caml_float_of_int_big:
 	# |x| >= 8388608
 	# x = m*8388608 + nとしてfloat_of_int(8388608)*m+float_of_int(n)を求める
 	# r2: x, r8: |x| or n, r9: FLAG, r10: 8388608, r11: -8388608, r12: temp
-	# f0: answer, f1: 8388608.0
-	addi	%r8 %r0 min_caml_float_0
-	fld	0(%r8) %f0
+	# f2: answer, f3: 8388608.0
+	fmov	%f2 %f0
 	addi	%r8 %r0 min_caml_float_int_c2
-	fld	0(%r8) %f1
+	flw	%f3 0(%r8)
 	add	%r8 %r0 %r11
 	add	%r9 %r0 %r0
 	addiu32	%r10 %r0 $8388608
-	addiu32	%r11 %r0 $4286578688 # -8388608
+	sub	%r11 %r0 %r10
 min_caml_float_of_int_big_loop:
 	# float_of_int(8388608)*mを求める
-	fadd	%f0 %f0 %f1
+	fadd	%f2 %f2 %f3
 	add	%r8 %r8 %r11
-	slt	%r12 %r10 %r8
-	beq	%r12 %r0 min_caml_float_of_int_big_loop
+	ble	%r10 %r8 min_caml_float_of_int_big_loop
 	# float_of_int(n)を求める
-	st	-1(%r29) %r2
-	fst	-2(%r29) %f0
+	sw	-1(%r29) %r2
+	fsw	-2(%r29) %f2
 	addi	%r29 %r29 $-3
-	st	0(%r29) %r31
+	sw	0(%r29) %r31
 	add	%r2 %r0 %r8
 	jal	min_caml_float_of_int
-	ld	0(%r29) %r31
+	lw	%r31 0(%r29)
 	addi	%r29 %r29 $3
-	fld	-2(%r29) %f1
-	ld	-1(%r29) %r2
+	flw	%f3 -2(%r29)
+	lw	%r2 -1(%r29)
 	# 足し算する
-	fadd	%f0 %f0 %f1
+	fadd	%f2 %f2 %f3
 	# FLAG
 	beq	%r9 %r0 min_caml_float_of_int_big_positive
-	fneg	%f0 %f0
+	fneg	%f2 %f2
 min_caml_float_of_int_big_positive:
 	jr	%r31
