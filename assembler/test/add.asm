63e2a7bf8390633cee7bcf70b1430866b56cccce
arcturu <spinsota@gmail.com>
2016-02-03 21:31:51 +0900

    [asm] modified test cases for 2nd arch.

--- assembler/test/add.asm
+++ assembler/test/add.asm
@@ -2,6 +2,12 @@
 .text
 .globl main
 main:
-addi %r1 %r0 $0xffff
+addi %r1 %r0 $0x0001
+send %r1
+addi %r1 %r1 $0x0002
+addi %r1 %r1 $0x0002
+addi %r1 %r1 $0x0002
+addi %r1 %r1 $0x0002
+addi %r1 %r1 $0x0002
 send %r1
 halt
