87c08fa890225c180ab680f65e5cc5a3eaed80c5
nonaka-is <meta.k.stb@gmail.com>
2016-02-15 10:28:08 +0900

    fmulのC実装を追加

---
+++ core/fpu/C/fmul.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+uint32_t fmul(uint32_t input1, uint32_t input2)
+{
+	uint32_t sign1,sign2,sign,expo1,expo2,tmp_expo1,tmp_expo2,expo,frac1,frac2,frac,result;
+	uint64_t tmp_frac;
+
+	sign1 = input1>>31;
+	sign2 = input2>>31;
+	expo1 = (input1<<1)>>24;
+	expo2 = (input2<<1)>>24;
+	frac1 = (input1<<9)>>9;
+	frac2 = (input2<<9)>>9;
+
+	sign = sign1^sign2;
+	tmp_expo1 = expo1 + expo2;
+	tmp_frac = (uint64_t)(0x800000 | frac1) * (uint64_t)(0x800000 | frac2);
+
+	if((tmp_frac>>47)==1){
+		tmp_expo2 = tmp_expo1 + 1;
+		frac = (0x7FFFFF) & ((uint32_t)(tmp_frac>>24));
+	}
+	else{
+		tmp_expo2 = tmp_expo1;
+		frac = (0x7FFFFF) & ((uint32_t)(tmp_frac>>23));
+	}
+
+	if((tmp_expo2>>8)|(tmp_expo2>>7)){
+		expo = tmp_expo2 - 127;
+		result = (sign<<31) | (expo<<23) | frac;
+	}
+	else{
+		result = 0;
+	}
+
+	return result;
+}
+
