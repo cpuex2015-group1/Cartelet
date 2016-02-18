15fea570c0cf0fcce8abb87d70071811c9047a69
nonaka-is <meta.k.stb@gmail.com>
2016-02-19 03:27:17 +0900

    fadd,fmulのC実装を修正

--- core/fpu/C/fmul.c
+++ core/fpu/C/fmul.c
@@ -2,7 +2,7 @@
 
 uint32_t fmul(uint32_t input1, uint32_t input2)
 {
-	uint32_t sign1,sign2,sign,expo1,expo2,tmp_expo1,tmp_expo2,expo,frac1,frac2,frac,result;
+	uint32_t sign1,sign2,sign,expo1,expo2,flag,tmp_expo1,tmp_expo2,expo,frac1,frac2,frac,result;
 	uint64_t tmp_frac;
 
 	sign1 = input1>>31;
@@ -16,6 +16,9 @@ uint32_t fmul(uint32_t input1, uint32_t input2)
 	tmp_expo1 = expo1 + expo2;
 	tmp_frac = (uint64_t)(0x800000 | frac1) * (uint64_t)(0x800000 | frac2);
 
+	if((expo1==0)||(expo2==0)) flag = 1;
+	else flag = 0;
+
 	if((tmp_frac>>47)==1){
 		tmp_expo2 = tmp_expo1 + 1;
 		frac = (0x7FFFFF) & ((uint32_t)(tmp_frac>>24));
@@ -25,12 +28,11 @@ uint32_t fmul(uint32_t input1, uint32_t input2)
 		frac = (0x7FFFFF) & ((uint32_t)(tmp_frac>>23));
 	}
 
-	if((tmp_expo2>>8)|(tmp_expo2>>7)){
-		expo = tmp_expo2 - 127;
-		result = (sign<<31) | (expo<<23) | frac;
-	}
+	if((flag==1)||(((tmp_expo2>>8)|(tmp_expo2>>7))==0)) result = (sign<<31);	
 	else{
-		result = 0;
+		expo = tmp_expo2 - 127;
+		expo = 0xFF & expo;
+		result = (sign<<31) | (expo<<23) | frac;
 	}
 
 	return result;
